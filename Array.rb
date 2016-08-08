a827b72a85a994a582a96b31520791b7c6e00744
Cheri <cheri.zewe@gmail.com>
2016-08-08 16:18:05 -0400

    array

---
+++ Array.rb
@@ -0,0 +1,15 @@
+def create_Mined_Minds_array
+	Array.new(100)
+end
+
+def array(number)
+	if number % 3 == 0 && number % 5 == 0
+		"Mined_Minds"
+	elsif number % 3 == 0
+		"Mined"
+	elsif number % 5 == 0
+		"Minds"
+	else
+		number
+	end
+end
---
+++ Test_Array.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require_relative "Array.rb"
+
+class Test_Mined_Minds_Kata <Minitest::Test
+
+	def test_1_returns_array
+		mined_minds_array = create_Mined_Minds_array
+		assert_equal(100, mined_minds_array.length)
+	end
+end
