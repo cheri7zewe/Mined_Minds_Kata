0f27e0235f67f76f9abcbe37e7e63e6875077812
Cheri <cheri.zewe@gmail.com>
2016-08-14 16:33:18 -0400

    Close but No Cigar

---
+++ cbnc.rb
@@ -0,0 +1,5 @@
+#Close but No Cigar Function 3756
+
+def close_numbers()
+	["2756", "4756", "3656", "3856", "3746", "3766", "3755", "3757"]
+end
---
+++ test_cbnc.rb
@@ -0,0 +1,9 @@
+#Close but No Cigar Function 3756
+
+require "minitest/autorun"
+require_relative "cbnc.rb"
+
+class Test_array <Minitest::Test
+	number = 3756
+	assert_equal(false, (number = close_numbers))
+end
